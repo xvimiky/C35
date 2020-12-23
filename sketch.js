be45a3585f077e34a9e0d7f79a3fb97e6d98abc7
xvimiky <72481545+xvimiky@users.noreply.github.com>
2020-12-22 19:55:22 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var ball, position;
+var db;
+
+function setup(){
+    createCanvas(500,500);
+
+    db = firebase.database();
+
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var ballPositionRef = db.ref('ball/position');
+    ballPositionRef.on("value", readPosition, showError);
+    //console.log(ballPositionRef);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    db.ref('ball/position').set({
+        'x': position.x + x,
+        'y': position.y + y
+    })
+    
+}
+
+function readPosition(data) {
+    position = data.val();
+    console.log(position);
+
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError() {
+    console.log("Error in the database");
+}
